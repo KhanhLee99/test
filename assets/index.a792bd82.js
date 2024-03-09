8a5b6cf15f8a0a04b9e9bfa703dc72b5ef2bfd56
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-10 00:32:55 +0700

    Updates

--- assets/index.a792bd82.js
+++
@@ -1,1 +0,0 @@
-import"./NotFound.e4f328fe.js";import{N as f}from"./NotFound.e4f328fe.js";import"./index.9437af3c.js";export{f as default};
---
+++ assets/user.api.6d34b757.js
@@ -0,0 +1,1 @@
+import{k as r}from"./index.398a14d7.js";const e={getProfile(){return r.get("me")},updateProfile(t){return r.put("user",t)},uploadAvatar(t){return r.post("user/upload-avatar",t,{headers:{"Content-Type":"multipart/form-data"}})}},u=e;export{u};
